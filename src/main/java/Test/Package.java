787d0220b934fec139306ef83ba323af628b69fc
Viktor Van Hoey <viktor.vanhoey@outlook.com>
2019-05-16 09:21:50 +0200

    Package

---
+++ src/main/java/Test/Package.java
@@ -0,0 +1,11 @@
+package Test;
+
+import com.github.rinde.rinsim.core.model.pdp.Parcel;
+import com.github.rinde.rinsim.core.model.pdp.ParcelDTO;
+
+public class Package extends Parcel {
+
+    public Package(ParcelDTO parcelDto) {
+        super(parcelDto);
+    }
+}
