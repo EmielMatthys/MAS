1be543432ccafa6dcd0fb2cf4dd96053cc7a716b
Emiel Matthys <emiel.matthys@student.kuleuven.be>
2019-05-20 11:37:50 +0200

    Oude shit idk

--- src/main/java/Test/Package.java
+++ src/main/java/Test/Package.java
@@ -1,11 +1,110 @@
 package Test;
 
+import com.github.rinde.rinsim.core.model.comm.*;
+import com.github.rinde.rinsim.core.model.pdp.PDPModel;
 import com.github.rinde.rinsim.core.model.pdp.Parcel;
 import com.github.rinde.rinsim.core.model.pdp.ParcelDTO;
+import com.github.rinde.rinsim.core.model.road.RoadModel;
+import com.github.rinde.rinsim.core.model.time.TickListener;
+import com.github.rinde.rinsim.core.model.time.TimeLapse;
+import com.github.rinde.rinsim.geom.Point;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Package extends Parcel {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class Package extends Parcel implements CommUser, TickListener {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(Package.class);
+    private static final double COMM_RANGE = 5;
+    private Optional<CommDevice> device;
+
+    private static final long TIME_WAIT = 2000;
+    private PackageState state = PackageState.BROADCAST;
+
+    enum PackageState {
+        BROADCAST, LISTENING, ASSIGNED
+    }
 
     public Package(ParcelDTO parcelDto) {
         super(parcelDto);
+    }
+
+    @Override
+    public Optional<Point> getPosition() {
+        return Optional.of(getRoadModel().getPosition(this));
+    }
+
+    @Override
+    public void setCommDevice(CommDeviceBuilder builder) {
+        this.device = Optional.of(builder.
+                setMaxRange(COMM_RANGE)
+                .build());
+    }
+
+    @Override
+    public void tick(TimeLapse timeLapse) {
+        RoadModel rm = getRoadModel();
+        PDPModel pm = getPDPModel();
+
+        if(state == PackageState.BROADCAST){
+            device.get().broadcast(new PackageMessage(PackageMessage.MessageType.CONTRACT_ANNOUNCE));
+            state = PackageState.LISTENING;
+        }
+        else if(state == PackageState.LISTENING){
+            ImmutableList<Message> messages = device.get().getUnreadMessages();
+            if(messages.size() == 0){
+                state = PackageState.BROADCAST;
+                return;
+            }
+
+            Message best = messages
+                    .stream()
+                    .sorted(new Comparator<Message>() {
+                        @Override
+                        public int compare(Message message, Message t1) {
+                            PackageMessage contents1 = ((PackageMessage) message.getContents());
+                            PackageMessage contents2 = ((PackageMessage) message.getContents());
+
+                            return Double.compare(contents1.getValue(), contents2.getValue());
+                        }
+                    })
+                    .collect(Collectors.toCollection(ArrayList::new))
+                    .get(0);
+
+            device.get().send(new PackageMessage(PackageMessage.MessageType.CONTRACT_ASSIGN), best.getSender());
+            state = PackageState.ASSIGNED;
+        }
+    }
+
+    @Override
+    public void afterTick(TimeLapse timeLapse) {}
+
+
+    public static class PackageMessage implements MessageContents {
+        enum MessageType{
+            CONTRACT_ANNOUNCE, CONTRACT_BID, CONTRACT_ASSIGN
+        }
+
+        private final MessageType type;
+        private final double value;
+
+        public PackageMessage(MessageType type){
+            this.type = type;
+            this.value = 0;
+        }
+
+        public PackageMessage(double value){
+            this.type = MessageType.CONTRACT_BID;
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
     }
 }
--- src/main/java/Test/SimpleAgent.java
+++ src/main/java/Test/SimpleAgent.java
@@ -3,6 +3,8 @@ package Test;
 import com.github.rinde.rinsim.core.model.comm.CommDevice;
 import com.github.rinde.rinsim.core.model.comm.CommDeviceBuilder;
 import com.github.rinde.rinsim.core.model.comm.CommUser;
+import com.github.rinde.rinsim.core.model.pdp.PDPModel;
+import com.github.rinde.rinsim.core.model.pdp.Parcel;
 import com.github.rinde.rinsim.core.model.pdp.Vehicle;
 import com.github.rinde.rinsim.core.model.pdp.VehicleDTO;
 import com.github.rinde.rinsim.core.model.road.CollisionGraphRoadModelImpl;
@@ -14,18 +16,25 @@ import com.github.rinde.rinsim.geom.PathNotFoundException;
 import com.github.rinde.rinsim.geom.Point;
 import com.google.common.base.Optional;
 import org.apache.commons.math3.random.RandomGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.swing.text.html.Option;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class SimpleAgent extends Vehicle implements TickListener, MovingRoadUser, CommUser {
 
+    protected static final Logger LOGGER = LoggerFactory.getLogger(SimpleAgent.class);
+
     private static final int VEHICLE_CAPACITY = 1;
     private static final double VEHICLE_SPEED = 10;
     private final RandomGenerator rng;
-    private Optional<Point> destination;
+    //private Optional<Point> destination;
     private Queue<Point> path;
     Optional<CommDevice> device;
+
+    Optional<Parcel> current;
 
     private final double range;
 
@@ -36,7 +45,7 @@ public class SimpleAgent extends Vehicle implements TickListener, MovingRoadUser
                 .capacity(VEHICLE_CAPACITY)
                 .build());
         this.rng = rng;
-        destination = Optional.absent();
+        //destination = Optional.absent();
         path = new LinkedList<>();
         this.range = 5;
         device = Optional.absent();
@@ -47,30 +56,40 @@ public class SimpleAgent extends Vehicle implements TickListener, MovingRoadUser
         return 1;
     }
 
-    void nextDestination() {
-        path = null;
 
-        do{
-            try {
-                destination = Optional.of(getRoadModel().getRandomPosition(rng));
-                path = new LinkedList<>(getRoadModel().getShortestPathTo(this,
-                        destination.get()));
-            }
-            catch(PathNotFoundException e) {}
-        } while(path == null);
-
-    }
 
     @Override
     protected void tickImpl(TimeLapse time) {
-        if (!destination.isPresent()) {
-            nextDestination();
+        //LOGGER.debug("SimpleAgent tick");
+        //TODO: roam the graph when unassigned
+
+        RoadModel rm = getRoadModel();
+        PDPModel pm = getPDPModel();
+
+        if(current.isPresent()){
+            final boolean inCargo = pm.containerContains(this, current.get());
+            // sanity check: if it is not in our cargo AND it is also not on the
+            // RoadModel, we cannot go to curr anymore.
+            if (!inCargo && !rm.containsObject(current.get())) {
+                current = Optional.absent();
+            } else if (inCargo) {
+                rm.moveTo(this, current.get().getDeliveryLocation(), time);
+                if (rm.getPosition(this).equals(current.get().getDeliveryLocation())) {
+                    // deliver when we arrive
+                    pm.deliver(this, current.get(), time);
+                }
+            } else {
+                // it is still available, go there as fast as possible
+                rm.moveTo(this, current.get(), time);
+                if (rm.equalPosition(this, current.get())) {
+                    // pickup customer
+                    pm.pickup(this, current.get(), time);
+                }
+            }
         }
 
-        getRoadModel().followPath(this, path, time);
+        if(device.get().getUnreadCount() > 0){
 
-        if (getRoadModel().getPosition(this).equals(destination.get())) {
-            nextDestination();
         }
     }
 
